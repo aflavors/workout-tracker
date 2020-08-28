ede63b111e002ca66d63b65b0ba825557c849563
Akeem Flavors <akeemflavors@gmail.com>
2020-08-28 16:29:57 -0400

    Added routes, sample entry, and app.listen

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,3 +17,31 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+//================================
+//SAMPLE ENTRY
+//================================
+db.Workout.create({type: "Running123", 
+                name: "Test Workout",
+                duration: 30,
+                weight: 0,
+                reps: 2,
+                sets: 0})
+.then(dbWorkout => {
+    console.log(dbWorkout);
+  })
+  .catch(({ message }) => {
+    console.log(message);
+  });
+
+//================================
+//ROUTES
+//================================
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
+
+//--------------------------------
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
